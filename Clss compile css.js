2580c48253dc27619e498c48d057adcf16965b2a
Strawberry-18 <cobra123prakash@gmail.com>
2025-02-10 12:25:10 +0530

    Update Clss compile css.js

--- Clss compile css.js
+++ Clss compile css.js
@@ -1,18 +1,19 @@
-
-    // Load JSON File
-    fetch("https://raw.githubusercontent.com/Strawberry-18/Clss/refs/heads/main/Clss%20all%20communication.json")
+    // JSON-ல் இருந்து CSS Properties Apply செய்யும் Function
+    fetch("https://raw.githubusercontent.com/Strawberry-18/Clcss/main/Clss.json")
         .then(response => response.json())
         .then(cssRules => {
             document.querySelectorAll("[data-class]").forEach(el => {
                 const classes = el.getAttribute("data-class").split(" ");
                 let styleString = "";
+
                 classes.forEach(cls => {
                     Object.values(cssRules).forEach(category => {
                         if (category[cls]) {
-                            styleString += category[cls];
+                            styleString += category[cls] + "; ";
                         }
                     });
                 });
+
                 el.style = styleString;
             });
         })
